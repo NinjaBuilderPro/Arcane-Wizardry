b859f0275fc6b7bd3aba0318482513afca8f2563
Ninjabuilderpro <Ninjabuilderpro@users.noreply.github.com>
2023-05-17 20:34:02 -0400

    WIP started adding capability for mana

---
+++ src/main/java/com/ninjabuilderpro/arcwizard/event/ModEvents.java
@@ -0,0 +1,56 @@
+package com.ninjabuilderpro.arcwizard.event;
+
+import com.ninjabuilderpro.arcwizard.arcwizard;
+import com.ninjabuilderpro.arcwizard.mana.PlayerMana;
+import com.ninjabuilderpro.arcwizard.mana.PlayerManaProvider;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = arcwizard.MODID)
+public class ModEvents {@SubscribeEvent
+    public static void onAttachCapabilitiesPlayer(AttachCapabilitiesEvent<Entity> event) {
+        if(event.getObject() instanceof Player) {
+            if(!event.getObject().getCapability(PlayerManaProvider.PLAYER_MANA).isPresent()) {
+                event.addCapability(new ResourceLocation(arcwizard.MODID, "properties"), new PlayerManaProvider());
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerCloned(PlayerEvent.Clone event) {
+        if(event.isWasDeath()) {
+            event.getOriginal().getCapability(PlayerManaProvider.PLAYER_MANA).ifPresent(oldStore -> {
+                event.getOriginal().getCapability(PlayerManaProvider.PLAYER_MANA).ifPresent(newStore -> {
+                    newStore.copyFrom(oldStore);
+                });
+            });
+        }
+    }
+
+    @SubscribeEvent
+    public static void onRegisterCapabilities(RegisterCapabilitiesEvent event) {
+        event.register(PlayerMana.class);
+    }
+
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if(event.side == LogicalSide.SERVER) {
+            event.player.getCapability(PlayerManaProvider.PLAYER_MANA).ifPresent(mana -> {
+                if(mana.getMana() > 0 && event.player.getRandom().nextFloat() < 0.01f) { // Once Every 5 Seconds on Avg
+                    mana.addMana(10);
+                    event.player.sendSystemMessage(Component.literal("Added Mana"));
+                }
+            });
+        }
+    }
+}
